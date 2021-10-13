11b08478f1c3f34a5de30f0dcbddc1c8be46f59c
bluwy <bjornlu.dev@gmail.com>
2021-10-13 10:32:37 +0800

    chore: update test

--- packages/svelte-fast-dimension/tests/Output.svelte
+++ packages/svelte-fast-dimension/tests/Output.svelte
@@ -2,7 +2,7 @@
 	let clientWidth, foo, w, h, w2, h2;
 </script>
 
-<div use:___resize on:fd:resize={(e) => { clientWidth = e.target.clientWidth }} />
-<div use:___resize on:fd:resize={(e) => { foo = e.target.clientWidth }} />
-<div use:___resize on:fd:resize={(e) => { clientWidth = e.target.clientWidth; w = e.target.clientHeight }}  />
-<div use:___resize on:fd:resize={(e) => { w = e.target.clientWidth; w = e.target.clientHeight; w = e.target.offsetWidth; w = e.target.offsetHeight }}    />
+<div use:___resize on:fd:resize={(e) => { clientWidth = e.target.clientWidth }}  />
+<div use:___resize on:fd:resize={(e) => { foo = e.target.clientWidth }}  />
+<div use:___resize on:fd:resize={(e) => { clientWidth = e.target.clientWidth; w = e.target.clientHeight }}   />
+<div use:___resize on:fd:resize={(e) => { w = e.target.clientWidth; w = e.target.clientHeight; w = e.target.offsetWidth; w = e.target.offsetHeight }}     />
